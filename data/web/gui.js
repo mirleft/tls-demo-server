129443b8e6a44f7d85abecca997522703aad10a4
Hannes Mehnert <hannes@mehnert.org>
2014-06-08 22:00:54 +0100

    we can also do notes btw

--- data/web/gui.js
+++ data/web/gui.js
@@ -1,5 +1,5 @@
 
-function msg (diagram, from, to, text, secret, ev1, tt) {
+function msg (from, to, text, secret, ev1, tt) {
     var typ = secret ? Diagram.LINETYPE.DOTTED : Diagram.LINETYPE.SOLID;
     return new Diagram.Signal(from, typ | (Diagram.ARROWTYPE.FILLED << 2), to, text, ev1, tt)
 }
@@ -14,23 +14,36 @@ function runme(diagram_div) {
     var tt = "tooltip here";
     var ev1y = function () { console.log("clicked you") }
     var ev1m = function () { console.log("clicked me") }
+    var right = Diagram.PLACEMENT.RIGHTOF;
+    var left = Diagram.PLACEMENT.LEFTOF;
 
-    var msgs = [ [ you, me, 'Client Hello', false, ev1y ] ,
-                 [ me, you, 'Server Hello', false, ev1m ] ,
-                 [ me, you, 'Certificate', false, ev1m ] ,
-                 [ me, you, 'Server Hello Done', false, ev1m ] ,
-                 [ you, me, 'Client Key Exchange', false, ev1y ] ,
-                 [ you, me, 'Change Cipher Spec', false, ev1y ],
-                 [ you, me, 'Finished', true, ev1y ],
-                 [ me, you, 'Change Cipher Spec', false, ev1m ],
-                 [ me, you, 'Finished', true, ev1m ],
-                 [ you, me, 'AD: GET /', true, ev1y ],
-                 [ me, you, 'AD: this site', true, ev1m ] ];
+    var msgs = [ [ 'msg', you, me, 'Client Hello', false, ev1y ] ,
+                 [ 'note', me, 'bla',  right ],
+                 [ 'msg', me, you, 'Server Hello', false, ev1m ] ,
+                 [ 'note', me, 'blubb', right ],
+                 [ 'msg', me, you, 'Certificate', false, ev1m ] ,
+                 [ 'note', me, 'blablubb', right ],
+                 [ 'msg', me, you, 'Server Hello Done', false, ev1m ] ,
+                 [ 'msg', you, me, 'Client Key Exchange', false, ev1y ] ,
+                 [ 'msg', you, me, 'Change Cipher Spec', false, ev1y ],
+                 [ 'msg', you, me, 'Finished', true, ev1y ],
+                 [ 'msg', me, you, 'Change Cipher Spec', false, ev1m ],
+                 [ 'msg', me, you, 'Finished', true, ev1m ],
+                 [ 'msg', you, me, 'AD: GET /', true, ev1y ],
+                 [ 'msg', me, you, 'AD: this site', true, ev1m ] ];
 
     for (var i = 0 ; i < msgs.length ; i++) {
         var m = msgs[i];
-        var sig = msg(diagram, m[0], m[1], m[2], m[3], m[4], tt)
-        diagram.addSignal(sig);
+        var typ = m[0];
+        if (typ == 'msg') {
+            var sig = msg(m[1], m[2], m[3], m[4], m[5], tt)
+            diagram.addSignal(sig);
+        } else if (typ == 'note') {
+            var not = new Diagram.Note(m[1], m[3], m[2]);
+            diagram.addSignal(not)
+        } else {
+            console.log("unknown msg")
+        }
     }
 
     var options = {
