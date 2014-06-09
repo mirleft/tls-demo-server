89bd2dce8ac49d652941d95c6d0b387986f7a0f3
Hannes Mehnert <hannes@mehnert.org>
2014-06-09 21:00:54 +0100

    move chap-msg-mapping to a function

--- data/web/gui.js
+++ data/web/gui.js
@@ -4,6 +4,37 @@ function msg (from, to, text, secret, ev1, tt) {
     return new Diagram.Signal(from, typ | (Diagram.ARROWTYPE.FILLED << 2), to, text, ev1, tt)
 }
 
+function lookupchap (msg) {
+    switch (msg) {
+        case 'Change Cipher Spec'  : return '.chapter-7-1'
+        case 'Hello Request'       : return '.chapter-7-4-1-1'
+        case 'Client Hello'        : return '.chapter-7-4-1-2'
+        case 'Server Hello'        : return '.chapter-7-4-1-3'
+        case 'Certificate'         : return '.chapter-7-4-2'
+        case 'Server Key Exchange' : return '.chapter-7-4-3'
+        case 'Server Hello Done'   : return '.chapter-7-4-5'
+        case 'Client Key Exchange' : return '.chapter-7-4-7'
+        case 'Finished'            : return '.chapter-7-4-9'
+        default                    : return '.chapter-10'
+    }
+}
+
+function rfc_cb (msg) {
+    var chap = lookupchap(msg);
+    return function () {
+        $('.rfc-chapter').hide() ;
+        $(chap).show('slow');
+    }
+}
+
+function process (diagram, in_record) {
+    if (in_record.event = 'message') {
+        //msg
+    } else {
+        //note
+    }
+}
+
 function runme(diagram_div) {
     $('.chapter-NONE').show();
 
@@ -30,36 +61,32 @@ function runme(diagram_div) {
     var me = diagram.getActor('Me');
 
     var tt = "tooltip here";
-    var rfc_cb = function (chap) {
-        return function () {
-            $('.rfc-chapter').hide() ;
-            $(chap).show('slow');
-        }
-    }
+
     var evn = function () { console.log("clicked note") }
     var right = Diagram.PLACEMENT.RIGHTOF;
     var left = Diagram.PLACEMENT.LEFTOF;
 
-    var msgs = [ [ 'msg', you, me, 'Client Hello', false, rfc_cb('.chapter-7-4-1-2') ] ,
+    var msgs = [ [ 'msg', you, me, 'Client Hello', false ] ,
                  [ 'note', me, 'bla',  right, evn ],
-                 [ 'msg', me, you, 'Server Hello', false, rfc_cb('.chapter-7-4-1-3') ] ,
+                 [ 'msg', me, you, 'Server Hello', false ] ,
                  [ 'note', me, 'blubb', right, evn ],
-                 [ 'msg', me, you, 'Certificate', false, rfc_cb('.chapter-7-4-2') ] ,
+                 [ 'msg', me, you, 'Certificate', false ] ,
                  [ 'note', me, 'blablubb\nblabla', right, evn ],
-                 [ 'msg', me, you, 'Server Hello Done', false, rfc_cb('.chapter-7-4-5') ] ,
-                 [ 'msg', you, me, 'Client Key Exchange', false, rfc_cb('.chapter-7-4-7') ] ,
-                 [ 'msg', you, me, 'Change Cipher Spec', false, rfc_cb('.chapter-7-1') ],
-                 [ 'msg', you, me, 'Finished', true, rfc_cb('.chapter-7-4-9') ],
-                 [ 'msg', me, you, 'Change Cipher Spec', false, rfc_cb('.chapter-7-1') ],
-                 [ 'msg', me, you, 'Finished', true, rfc_cb('.chapter-7-4-9') ],
-                 [ 'msg', you, me, 'AD: GET /', true, rfc_cb('.chapter-10') ],
-                 [ 'msg', me, you, 'AD: this site', true, rfc_cb('.chapter-10') ] ];
+                 [ 'msg', me, you, 'Server Hello Done', false ] ,
+                 [ 'msg', you, me, 'Client Key Exchange', false ] ,
+                 [ 'msg', you, me, 'Change Cipher Spec', false ],
+                 [ 'msg', you, me, 'Finished', true ],
+                 [ 'msg', me, you, 'Change Cipher Spec', false ],
+                 [ 'msg', me, you, 'Finished', true ],
+                 [ 'msg', you, me, 'AD: GET /', true ],
+                 [ 'msg', me, you, 'AD: this site', true ] ];
 
     for (var i = 0 ; i < msgs.length ; i++) {
         var m = msgs[i];
         var typ = m[0];
         if (typ == 'msg') {
-            var sig = msg(m[1], m[2], m[3], m[4], m[5])
+            var cb = rfc_cb(m[3]);
+            var sig = msg(m[1], m[2], m[3], m[4], cb)
             diagram.addSignal(sig);
         } else if (typ == 'note') {
             var not = new Diagram.Note(m[1], m[3], m[2], m[4], tt);
