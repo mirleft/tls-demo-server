7e679bf268573db4a09504adfe5caea685975e76
pqwy <david@numm.org>
2014-06-10 01:47:24 +0100

    tweak dispatcher

--- unikernel.ml
+++ unikernel.ml
@@ -52,41 +52,45 @@ struct
     with _ -> "text/plain"
 
   let response path =
-    let header =
-      let ct = content_type path in
-      let h = Cohttp.Header.init_with "Content-type" ct in
-      Cohttp.Header.add h "Connection" "Keep-Alive"
-    in
-    Http.Response.make ~status:`OK ~headers:header ()
+    Http.Response.make
+      ~status:`OK
+      ~headers:(Cohttp.Header.of_list [
+        "Content-type" , content_type path
+      ; "Connection"   , "Keep-Alive"
+      ]) ()
 
-  let dispatch kv path trace =
+  let dispatch (c, kv, _, trace) path =
+    let traces = trace () in
+    lwt _ = save_traces c traces in
     let resp = response path in
     try_lwt
-      lwt data = match path with
-                 | "/diagram.json" -> return (String.concat "\n" (trace ()))
-                 | "/diagram.txt"  -> return (String.concat "\n" (trace ()))
-                 | s               -> read_kv kv s
+      lwt data =
+        match path with
+        | "/diagram.json"
+        | "/diagram.txt" -> return (render_traces traces)
+        | s              -> read_kv kv s
       in
       return (resp, (Body.of_string data))
     with _ ->
       return (Http.Response.make ~status:`Internal_server_error (),
               Body.of_string "<html><head>Server Error</head></html>")
 
-  let handle c kv trace tls conn req body =
+  let handle (_, kv, tls, _ as ctx) conn req body =
     let path = Uri.path req.Http.Request.uri in
     match path with
     | "/rekey"        -> TLS.rekey tls >> return (response "/rekey.txt",
                                                   Body.of_string "intentionally left blank")
-    | "/"             -> dispatch kv "/index.html" trace
-    | s               -> dispatch kv s trace
+    | "/"             -> dispatch ctx "/index.html"
+    | s               -> dispatch ctx s
 
   let upgrade c conf kv tcp =
     let trace, get_trace = make_tracer () in
     TLS.server_of_tcp_flow ~trace conf tcp >>= function
       | `Error _ -> fail (Failure "tls init")
       | `Ok tls  ->
+          let ctx = (c, kv, tls, get_trace) in
           let open Http.Server in
-          listen { callback = handle c kv get_trace tls ; conn_closed = fun _ () -> () } tls
+          listen { callback = handle ctx; conn_closed = fun _ () -> () } tls
 
   let start c stack kv =
     lwt cert = X509.certificate kv `Default in
