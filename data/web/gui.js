e3fad4eea778b84f493bf44693f9a14b2f995f2b
Hannes Mehnert <hannes@mehnert.org>
2014-06-11 10:46:46 +0100

    use live data condense multiple messages of same type into a single signal use simple layouting (hand just doesn't scale)

--- data/web/gui.js
+++ data/web/gui.js
@@ -6,92 +6,110 @@ function msg (from, to, text, ev1, tt) {
 
 function lookupchap (msg) {
     switch (msg) {
-        case 'Change Cipher Spec'  : return '.chapter-7-1'
-        case 'Hello Request'       : return '.chapter-7-4-1-1'
-        case 'Client Hello'        : return '.chapter-7-4-1-2'
-        case 'Server Hello'        : return '.chapter-7-4-1-3'
-        case 'Certificate'         : return '.chapter-7-4-2'
-        case 'Server Key Exchange' : return '.chapter-7-4-3'
-        case 'Server Hello Done'   : return '.chapter-7-4-5'
-        case 'Client Key Exchange' : return '.chapter-7-4-7'
-        case 'Finished'            : return '.chapter-7-4-9'
-        default                    : return '.chapter-10'
+        case 'ChangeCipherSpec'  : return '.chapter-7-1'
+        case 'HelloRequest'      : return '.chapter-7-4-1-1'
+        case 'ClientHello'       : return '.chapter-7-4-1-2'
+        case 'ServerHello'       : return '.chapter-7-4-1-3'
+        case 'Certificate'       : return '.chapter-7-4-2'
+        case 'ServerKeyExchange' : return '.chapter-7-4-3'
+        case 'ServerHelloDone'   : return '.chapter-7-4-5'
+        case 'ClientKeyExchange' : return '.chapter-7-4-7'
+        case 'Finished'          : return '.chapter-7-4-9'
+        default                  : return '.chapter-10'
     }
 }
 
-function rfc_cb (pre, msg, data) {
+
+function clear_element (element) {
+    while (element.hasChildNodes())
+        element.removeChild(element.firstChild)
+}
+
+function rfc_cb (ul, msg, data) {
     var chap = lookupchap(msg);
     return function () {
         $('.rfc-chapter').hide() ;
         $(chap).show('slow');
-        pre.innerHTML = data;
+        clear_element(ul);
+        for (var i = 0 ; i < data.length ; i++) {
+            var li = document.createElement('li');
+            var datum = data[i];
+            var dat = datum[0] != '' ? datum[0] + ': ' : ''
+            li.innerHTML = dat + datum[1];
+            ul.appendChild(li);
+        }
     }
 }
 
 function handle_sec (from, msg) {
-    if (msg == 'Change Cipher Spec')
+    if (msg == 'ChangeCipherSpec')
         from.secure = true
 }
 
 var updating = false;
 
-function process (diagram, details_pre, in_record) {
-    var me = diagram.getActor('Me');
+function process (me, you, in_record) {
     var txt = in_record.message;
     if (in_record.event == 'message') {
         //msg
-        var you = diagram.getActor('You');
         var from = me ;
         var to = you ;
         if (in_record.direction == 'in') {
             from = you ; to = me ;
         }
-        var cb = rfc_cb(details_pre, txt, in_record.data);
-        var sig = msg(from, to, txt, cb);
+        var r = msg(from, to, txt);
         handle_sec(from, txt);
-        diagram.addSignal(sig);
-    } else {
+        return r;
+    } //else {
         //note
-        var not = new Diagram.Note(me, Diagram.PLACEMENT.RIGHTOF, txt);
-        diagram.addSignal(not)
+     //   var not = new Diagram.Note(me, Diagram.PLACEMENT.RIGHTOF, txt);
+     //   diagram.addSignal(not)
+    //}
+}
+
+function insertSignals (diagram, details_ul, signals, data) {
+
+    var last = null;
+
+    for ( var i = 0 ; i < signals.length ; i++ ) {
+        var now = signals[i];
+        var txt = now.message;
+        if (last != null && last.message == txt && last.actorA == now.actorA) {
+            last.message = last.message + "*";
+        } else {
+            var cb = rfc_cb(details_ul, txt, data[i].data);
+            now.ev1 = cb;
+            diagram.addSignal(now);
+        }
+        last = now;
     }
 }
 
-function getData (diagram_div, details_pre) {
-  $.ajax({ url: "/diagram.txt" }).done( function( data ) {
+function getData (diagram_div, details_ul) {
+  $.ajax({ url: "/diagram.json" }).done( function( data ) {
       console.log( "received data:", data.length );
       var diagram = new Diagram();
-      diagram.getActor('You'); diagram.getActor('Me');
-      var msgs = [ { "event": "message", "message": "Client Hello", "direction": "in", "data": "version: TLS_1_2\nciphersuites: A, B, C, D\nrandom: 00112233445566778899001122334455\n\t66778899001122334455667788990011" },
-                 { "event": "note",    "message": "bla" },
-                 { "event": "message", "message": "Server Hello", "direction": "out" },
-                 { "event": "note",    "message": "blubb" },
-                 { "event": "message", "message": "Certificate", "direction": "out" },
-                 { "event": "note",    "message": "blablubb" },
-                 { "event": "message", "message": "Server Hello Done", "direction": "out" },
-                 { "event": "message", "message": "Client Key Exchange", "direction": "in" },
-                 { "event": "message", "message": "Change Cipher Spec", "direction": "in" },
-                 { "event": "message", "message": "Finished", "direction": "in" },
-                 { "event": "message", "message": "Change Cipher Spec", "direction": "out" },
-                 { "event": "message", "message": "Finished", "direction": "out" },
-                 { "event": "message", "message": "AD: GET /", "direction": "in" },
-                 { "event": "message", "message": "AD: this site", "direction": "out" } ]
-      for (var i = 0 ; i < msgs.length ; i++) {
-          process(diagram, details_pre, msgs[i]);
-      }
+      var you = diagram.getActor('You');
+      var me = diagram.getActor('Me');
+
+      var signals = _.map(data, function (x) { return process(me, you, x) });
+      insertSignals(diagram, details_ul, signals, data);
+
       var options = {
-          theme: 'hand',
-          scale: 1.2
+          theme: 'simple'
       };
 
       diagram_div.innerHTML='';
-      details_pre.innerHTML='';
+      clear_element(details_ul);
       $('.rfc-chapter').hide() ;
       $('.chapter-NONE').show();
 
       // Draw
       diagram.drawSVG(diagram_div, options);
       updating = false;
+      var req = document.getElementById("request")
+      req.value = 'renegotiate!';
+      req.disabled = false;
   });
 }
 
@@ -99,16 +117,18 @@ function initialise () {
     $('.chapter-NONE').show();
 
     var diagram_div = document.getElementById('diagram');
-    var details_pre = document.getElementById('details-pre');
+    var details_ul = document.getElementById('details');
 
-    getData(diagram_div, details_pre);
+    getData(diagram_div, details_ul);
 
     var req = document.getElementById("request")
     request.onclick = function () {
         if (! updating) {
             updating = true;
+            req.value = 'renegotiating...';
+            req.disabled = true;
             $.ajax({ url: "/rekey" }).done( function () {
-                setTimeout(function () { return getData(diagram_div, details_pre); }, 1000)
+                setTimeout(function () { return getData(diagram_div, details_ul); }, 1000)
             })
         }
     }
