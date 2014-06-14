d2ba7a0ef1d4da521e89468d7cd7d343e85892a7
pqwy <david@numm.org>
2014-06-14 18:11:42 +0100

    a little jquery

--- data/web/gui.js
+++ data/web/gui.js
@@ -19,29 +19,19 @@ function lookupchap (msg) {
     }
 }
 
-
-function clear_element (element) {
-    while (element.hasChildNodes())
-        element.removeChild(element.firstChild)
-}
-
-function rfc_cb (sig, ul, msg, moredata) {
+function rfc_cb (sig, msg, moredata) {
     var chap = lookupchap(msg);
-    for (var i = 0 ; i < moredata.length ; i++) {
-        var datum = moredata[i];
-        var dat = datum[0] != '' ? datum[0] + ': ' : ''
+    _.each (moredata, function (datum) {
+        var dat = datum[0] != '' ? datum[0] + ': ' : '' ;
         var txt = dat + datum[1];
-        sig.data.push(txt);
-    }
+        sig.data.push (txt);
+    })
     return function () {
         $('.rfc-chapter').hide() ;
-        $(chap).show('slow');
-        clear_element(ul);
-        for (var i = 0 ; i < sig.data.length ; i++) {
-            var li = document.createElement('li');
-            li.innerHTML = sig.data[i];
-            ul.appendChild(li);
-        }
+        $(chap).show('fast');
+        $('#details').empty().append(
+          _.map (sig.data, function (d) { return $("<li>").append (d) })
+        )
     }
 }
 
@@ -72,7 +62,7 @@ function process (me, you, in_record) {
     }
 }
 
-function insertSignals (diagram, details_ul, signals, data) {
+function insertSignals (diagram, signals, data) {
 
     var last = null;
 
@@ -82,13 +72,13 @@ function insertSignals (diagram, details_ul, signals, data) {
             var txt = now.message;
             if (last != null && last.actorA == now.actorA &&
                 (last.message == txt || last.message == txt + '*' )) {
-                var cb = rfc_cb(last, details_ul, txt, data[i].data);
+                var cb = rfc_cb(last, txt, data[i].data);
                 last.ev1 = cb;
                 if (last.message == txt) {
                     last.message = last.message + "*";
                 }
             } else {
-                var cb = rfc_cb(now, details_ul, txt, data[i].data);
+                var cb = rfc_cb(now, txt, data[i].data);
                 now.ev1 = cb;
                 diagram.addSignal(now);
                 last = now;
@@ -99,40 +89,34 @@ function insertSignals (diagram, details_ul, signals, data) {
     }
 }
 
-function getData (diagram_div, details_ul) {
+function getData () {
   $.ajax({ url: "/diagram.json" }).done( function( data ) {
       var diagram = new Diagram();
       var you = diagram.getActor('Client');
       var me = diagram.getActor('Server');
 
       var signals = _.map(data, function (x) { return process(me, you, x) });
-      insertSignals(diagram, details_ul, signals, data);
+      insertSignals(diagram, signals, data);
 
       var options = {
           theme: 'simple'
       };
 
-      diagram_div.innerHTML='';
-      clear_element(details_ul);
+      $('#diagram, #details').empty ();
       $('.rfc-chapter').hide() ;
       $('.chapter-NONE').show();
 
       // Draw
-      diagram.drawSVG(diagram_div, options);
+      diagram.drawSVG($('#diagram')[0], options);
       updating = false;
-      var req = document.getElementById("request")
-      req.value = 'renegotiate!';
-      req.disabled = false;
+      $('#request').val('renegotiate!').attr('disabled', false);
   });
 }
 
 function initialise () {
     $('.chapter-NONE').show();
 
-    var diagram_div = document.getElementById('diagram');
-    var details_ul = document.getElementById('details');
-
-    getData(diagram_div, details_ul);
+    getData();
 
     var req = document.getElementById("request")
     request.onclick = function () {
@@ -141,7 +125,7 @@ function initialise () {
             req.value = 'renegotiating...';
             req.disabled = true;
             $.ajax({ url: "/rekey" }).done( function () {
-                setTimeout(function () { return getData(diagram_div, details_ul); }, 300)
+                setTimeout(function () { return getData(); }, 300)
             })
         }
     }
