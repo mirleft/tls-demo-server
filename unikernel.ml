d1fb5fedd85019f84a629d16c731d66875cabee4
pqwy <david@numm.org>
2014-06-14 16:09:52 +0100

    sexp->json is a module

--- unikernel.ml
+++ unikernel.ml
@@ -14,97 +14,102 @@ let rec map_partial ~f = function
       | None   ->      map_partial ~f xs
       | Some y -> y :: map_partial ~f xs
 
-open Sexplib.Sexp
 
-let bytes_dump = function
-  | List bs ->
-      let repr =
-        List.map (fun (List bytes) ->
-          `String (List.map (fun (Atom x) -> x) bytes |> String.concat " "))
-        bs in
-      `List repr
+module Traces_out = struct
 
-let rec flatten_sexp = function
-  | Atom x  -> x
-  | List xs -> List.map flatten_sexp xs |> String.concat " "
+  open Sexplib.Sexp
 
-let rec implode = function
-  | []    -> ""
-  | x::xs -> (Char.escaped x) ^ (implode xs)
+  let bytes_dump = function
+    | List bs ->
+        let repr =
+          List.map (fun (List bytes) ->
+            `String (List.map (fun (Atom x) -> x) bytes |> String.concat " "))
+          bs in
+        `List repr
 
-let hex_sexp_to_string xs =
-  List.map (fun (List hex) ->
-            implode (List.map (fun (Atom x) -> char_of_int (Scanf.sscanf x "%x" (fun x -> x))) hex))
-           xs |> String.concat ""
+  let rec flatten_sexp = function
+    | Atom x  -> x
+    | List xs -> List.map flatten_sexp xs |> String.concat " "
 
-let app_data_to_string bytes =
-  [ `List [ `String "" ; `String (hex_sexp_to_string bytes)]]
+  let rec implode = function
+    | []    -> ""
+    | x::xs -> (Char.escaped x) ^ (implode xs)
 
-let dict_dump = function
-  | List [Atom tag ; Atom value] -> `List [ `String tag ; `String value ]
-  | List [Atom tag ; List value] ->
-     `List [ `String tag ; `String ((List.map flatten_sexp value) |> String.concat ", ") ]
-  | List [Atom tag ; x ] -> `List [ `String tag ; `String (": broken2 : " ^ (to_string_hum x)) ]
-  | List stuff -> `List [ `String "" ;
-                          `String (List.map flatten_sexp stuff |> String.concat "\n") ]
-  | x -> `List [ `String "unknown" ; `String ("broken3 : " ^ (to_string_hum x)) ]
+  let hex_sexp_to_string xs =
+    List.map (fun (List hex) ->
+              implode (List.map (fun (Atom x) -> char_of_int (Scanf.sscanf x "%x" (fun x -> x))) hex))
+            xs |> String.concat ""
 
-let json_of_trace sexp : Yojson.json option =
-  let record ~dir ~ty ~bytes = `Assoc [
-      "event"     , `String "message"
-    ; "direction" , `String dir
-    ; "message"   , `String ty
-    ; "data"      , `List bytes
-  ]
-  and state ~dir ~ver:(Atom v) ~mach ~rekey =
-    `Assoc [
-        "event"     , `String "state"
+  let app_data_to_string bytes =
+    [ `List [ `String "" ; `String (hex_sexp_to_string bytes)]]
+
+  let dict_dump = function
+    | List [Atom tag ; Atom value] -> `List [ `String tag ; `String value ]
+    | List [Atom tag ; List value] ->
+      `List [ `String tag ; `String ((List.map flatten_sexp value) |> String.concat ", ") ]
+    | List [Atom tag ; x ] -> `List [ `String tag ; `String (": broken2 : " ^ (to_string_hum x)) ]
+    | List stuff -> `List [ `String "" ;
+                            `String (List.map flatten_sexp stuff |> String.concat "\n") ]
+    | x -> `List [ `String "unknown" ; `String ("broken3 : " ^ (to_string_hum x)) ]
+
+  let json_of_trace sexp : Yojson.json option =
+    let record ~dir ~ty ~bytes = `Assoc [
+        "event"     , `String "message"
       ; "direction" , `String dir
-      ; "version"   , `String v
-      (* XXX decode machina into something usable *)
-      ; "machina"   , `String (to_string mach)
+      ; "message"   , `String ty
+      ; "data"      , `List bytes
     ]
-  and note ~msg ~data =
-    `Assoc [
-        "event"     , `String "note"
-      ; "message"   , `String msg
-      ; "data"      , `String data
-    ]
-  in
-  match sexp with
-  | List [Atom tag; List sexps] ->
-    ( match (tag, sexps) with
-      | "handshake-in", [Atom ty; List data ] ->
-         Some (record ~dir:"in" ~ty ~bytes:(List.map dict_dump data))
-      | "handshake-out", [Atom ty; List data] ->
-         Some (record ~dir:"out" ~ty ~bytes:(List.map dict_dump data))
-      | "change-cipher-spec-in", _ ->
-         Some (record ~dir:"in" ~ty:"ChangeCipherSpec" ~bytes:[])
-      | "change-cipher-spec-out", _ ->
-         Some (record ~dir:"out" ~ty:"ChangeCipherSpec" ~bytes:[])
-      | "application-data-in", bytes ->
-         Some (record ~dir:"in" ~ty:"ApplicationData" ~bytes:(app_data_to_string bytes))
-      | "application-data-out", bytes ->
-         Some (record ~dir:"out" ~ty:"ApplicationData" ~bytes:(app_data_to_string bytes))
-      | "master-secret", bytes ->
-         Some (note ~msg:"MasterSecret" ~data:(flatten_sexp (List bytes)))
-      | _ -> None
-    )
-  | List [Atom tag; Atom ty] -> (* happens for empty messages: ServerHelloDone / HelloRequest *)
-     ( match tag with
-      | "handshake-in" ->
-         Some (record ~dir:"in" ~ty ~bytes:([]))
-      | "handshake-out" ->
-         Some (record ~dir:"out" ~ty ~bytes:([]))
-      | "version" ->
-         Some (note ~msg:ty ~data:"")
-      | "cipher" ->
-         Some (note ~msg:"Cipher" ~data:ty)
-      | _ -> None )
-  | _ -> None
+    and state ~dir ~ver:(Atom v) ~mach ~rekey =
+      `Assoc [
+          "event"     , `String "state"
+        ; "direction" , `String dir
+        ; "version"   , `String v
+        (* XXX decode machina into something usable *)
+        ; "machina"   , `String (to_string mach)
+      ]
+    and note ~msg ~data =
+      `Assoc [
+          "event"     , `String "note"
+        ; "message"   , `String msg
+        ; "data"      , `String data
+      ]
+    in
+    match sexp with
+    | List [Atom tag; List sexps] ->
+      ( match (tag, sexps) with
+        | "handshake-in", [Atom ty; List data ] ->
+          Some (record ~dir:"in" ~ty ~bytes:(List.map dict_dump data))
+        | "handshake-out", [Atom ty; List data] ->
+          Some (record ~dir:"out" ~ty ~bytes:(List.map dict_dump data))
+        | "change-cipher-spec-in", _ ->
+          Some (record ~dir:"in" ~ty:"ChangeCipherSpec" ~bytes:[])
+        | "change-cipher-spec-out", _ ->
+          Some (record ~dir:"out" ~ty:"ChangeCipherSpec" ~bytes:[])
+        | "application-data-in", bytes ->
+          Some (record ~dir:"in" ~ty:"ApplicationData" ~bytes:(app_data_to_string bytes))
+        | "application-data-out", bytes ->
+          Some (record ~dir:"out" ~ty:"ApplicationData" ~bytes:(app_data_to_string bytes))
+        | "master-secret", bytes ->
+          Some (note ~msg:"MasterSecret" ~data:(flatten_sexp (List bytes)))
+        | _ -> None
+      )
+    | List [Atom tag; Atom ty] -> (* happens for empty messages: ServerHelloDone / HelloRequest *)
+      ( match tag with
+        | "handshake-in" ->
+          Some (record ~dir:"in" ~ty ~bytes:([]))
+        | "handshake-out" ->
+          Some (record ~dir:"out" ~ty ~bytes:([]))
+        | "version" ->
+          Some (note ~msg:ty ~data:"")
+        | "cipher" ->
+          Some (note ~msg:"Cipher" ~data:ty)
+        | _ -> None )
+    | _ -> None
 
-let render_traces sexps =
-  Yojson.to_string @@ `List (map_partial ~f:json_of_trace sexps)
+  let render_traces sexps =
+    Yojson.to_string @@ `List (map_partial ~f:json_of_trace sexps)
+
+end
 
 module Main (C  : CONSOLE)
             (S  : STACKV4)
@@ -164,7 +169,7 @@ struct
     try_lwt
       lwt data =
         match path with
-        | "/diagram.json" -> return (render_traces traces)
+        | "/diagram.json" -> return (Traces_out.render_traces traces)
         | s               -> read_kv kv s
       in
       return (resp, (Body.of_string data))
